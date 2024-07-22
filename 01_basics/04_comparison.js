264ef0d10a8fca85b467acf84991194cc0f07905
Dewansh Pratap Singh <114020036+Dewansh225@users.noreply.github.com>
2024-07-22 15:08:21 +0000

    comparison operator

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+// Try to avoide this type of conversion because confusing
+
+
+
+// === 
+
+console.log("2" === 2);
